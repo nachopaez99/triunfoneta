1a27b98f2d83f7f467d216f8ecb1c44114d385b6
nachopaez99 <nachopaez99@gmail.com>
2026-06-29 09:42:53 -0300

    se reduce el nro de letras en el nombre de la figurita de 23 a 20

--- src/components/sticker/PersonalStickerCard.jsx
+++ src/components/sticker/PersonalStickerCard.jsx
@@ -27,7 +27,7 @@ const isLegend =
         <img src={imageUrl} alt={displayName} />
       </div>
 
-      <h3 className={displayName.length > 23? "opened-sticker__name-" : "opened-sticker__name-max" }>{displayName}</h3>
+      <h3 className={displayName.length > 20? "opened-sticker__name-" : "opened-sticker__name-max" }>{displayName}</h3>
 
       {isLegend && <div className="sticker-legend">Legendaria</div>}
 
